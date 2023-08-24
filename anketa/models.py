cb7f516d1171ef22ae77384217f59bbdfaac5677
Maksim Karginov <ddmd4698@gmail.com>
2023-08-24 11:13:07 +0300

    add models Respondent, Anketa

--- anketa/models.py
+++ anketa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Respondent(models.Model):
+    """
+    Респондент для анкеты
+    """
+
+    # Fields
+    last_name = models.CharField(max_length=20, help_text="Фамилия респондента")
+
+class Anketa(models.Model):
+    """
+    Анкета
+    """
+
+    # Fields
+    name = models.CharField(max_length=20, help_text="Имя анкеты")
+    discription = models.CharField(max_length=20, help_text="Описание анкеты")
