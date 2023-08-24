defb2a80455d0549cc9cb39d3976b6c2818f501e
Maksim Karginov <ddmd4698@gmail.com>
2023-08-24 11:43:52 +0300

    add models Vopros, Variant_Otveta, Otvet_na_vopros

--- anketa/models.py
+++ anketa/models.py
@@ -15,5 +15,34 @@ class Anketa(models.Model):
     """
 
     # Fields
-    name = models.CharField(max_length=20, help_text="Имя анкеты")
+    name = models.CharField(max_length=20, help_text="Имя(название) анкеты")
     discription = models.CharField(max_length=20, help_text="Описание анкеты")
+
+    # author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+class Vopros(models.Model):
+    """
+    Вопрос для анкеты
+    """
+
+    # Fields
+    questionnaire_id = models.CharField(max_length=20, help_text="id Анкеты")
+    name = models.CharField(max_length=20, help_text="Имя(название) вопроса")
+
+class Variant_Otveta(models.Model):
+    """
+    Варианты ответов для анкеты
+    """
+
+    # Fields
+    name = models.CharField(max_length=20, help_text="Имя(название) ответов")
+    question_id = models.CharField(max_length=20, help_text="id вопроса")
+
+class Otvet_na_vopros(models.Model):
+    """
+    Ответы на вопросы для анкеты
+    """
+
+    # Fields
+    answer_id = models.CharField(max_length=20, help_text="id ответа")
+    respondent_id = models.CharField(max_length=20, help_text="id респондента") 
